e1f61c01fad96702a568b9d2c089c2e0eb36df23
Natan Felles <natanfelles@gmail.com>
2021-09-21 19:30:16 -0300

    Update layout-example

--- views/examples/layout-example.php
+++ views/examples/layout-example.php
@@ -5,14 +5,25 @@
 $view->extends('example');
 $view->block('contents');
 ?>
-    <div class="row">
+
+    <div class="container row pad-3" style="background: purple">
         <div class="box">Logo</div>
         <div class="box">foo</div>
         <div class="box">foo 2</div>
+        <div class="box">
+            <a href="#">foo 3</a>
+        </div>
     </div>
 
     <div class=" container row">
         <main class="box-9">
+            <h2>Foo</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h3>Fooss</h3>
+            <h2>Bar</h2>
+            <h2>Baz</h2>
+            <hr>
             <h2>Foo</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -29,10 +40,22 @@ $view->block('contents');
                     <li>aaaa</li>
                 </ul>
             </div>
+            <div class="list hoverable border mar-t-3">
+                <a href="#">aaaa</a>
+                <a href="#">aaaa</a>
+                <a href="#">aaaa</a>
+                <a href="#">aaaa</a>
+            </div>
+            <div class="list hoverable border mar-t-3">
+                <a href="#">aaaa</a>
+                <a href="#">aaaa</a>
+                <a href="#">aaaa</a>
+                <a href="#">aaaa</a>
+            </div>
         </aside>
     </div>
-    <hr>
-    <footer class="container row margin-top-x">
+<!--    <hr>-->
+    <footer class="container row">
         <div class="box-3">
             <h3>Foo</h3>
             <ul>
--- views/index.php
+++ views/index.php
@@ -16,6 +16,9 @@ $view->block('contents');
             <a href="/examples/forms.html">forms</a>
         </li>
         <li>
+            <a href="/examples/layout-example.html">layout-example</a>
+        </li>
+        <li>
             <a href="/examples/tables.html">tables</a>
         </li>
         <li>
